6cc951e77f75c649df8ddd26f33a2d48e8ecd445
AccL2 <162718803+AccL2@users.noreply.github.com>
2026-05-31 16:03:24 +0200

    Update memoria_de.py

--- memoria_de.py
+++ memoria_de.py
@@ -344,7 +344,7 @@ def formatear_lineas(texto):
 
 # URLs
 SHEET_URL   = "https://docs.google.com/spreadsheets/d/1hpP0J5qRrbx5p9W2nHWsoTDBA9hhvLZYblaU12Ln3w4/export?format=csv"
-WEB_APP_URL = "https://script.google.com/macros/s/AKfycbzUjhiwydEHUGfYaEJOXpvJf-00D1Yx3jHltLgGPpCXXc_08dL_4fugUmmY7u7EmXgM/exec"
+WEB_APP_URL = "https://script.google.com/macros/s/AKfycbwtX3nZ-NsC3QP9PkIw0WBrfoSA90Tca5DVAN9UGp2Z62nHrsc3D3PA1c8GtMNbBdYY/exec"
 
 @st.cache_data(ttl=2)
 def cargar_datos_web():
